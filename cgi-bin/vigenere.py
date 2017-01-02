1b4db2278e62fc59875ce85b17d6fa35481584a6
oprikazchikov <oprikazchikov@gmail.com>
2017-01-03 00:58:24 +0500

    убрал .idea

--- cgi-bin/vigenere.py
+++ cgi-bin/vigenere.py
@@ -5,6 +5,7 @@ import caesar
 class Vigenere:
     """Метод Виженера"""
 
-    def __init__(self, data="АБВГ ДЕЁЖ"):
+    def __init__(self, data="АБВГ ДЕЁЖ", key="ТЕСТ"):
         """Инициализация"""
         self.data = data
+        self.key = key
