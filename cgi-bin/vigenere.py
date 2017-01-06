fe6c2111bd2b839c80d1de4b943f784557c6988d
oprikazchikov <oprikazchikov@gmail.com>
2017-01-06 16:51:05 +0500

    Рабочий алгоритм шифрования методом Виженера

--- cgi-bin/vigenere.py
+++ cgi-bin/vigenere.py
@@ -1,12 +1,33 @@
-# coding=utf-8
-import caesar
+# -*- coding: utf-8 -*-
 
 
-class Vigenere:
-    """Метод Виженера"""
+first = 0
+last = 0
+for i in range(255, 10000):
+    if chr(i) == 'А':
+        first = i
+    elif chr(i) == 'Я':
+        last = i + 1
+        break
+d = [chr(i) for i in range(first, last)]  #(127)]
+dl = len(d)
 
-    def __init__(self, data="АБВГ ДЕЁЖ", key="ТЕСТ"):
-        """Инициализация"""
-        self.data = data
-        self.key = key
+prepval = lambda val: zip(range(0, len(val)), val)
 
+enc = lambda ch, key: (ch + key) % dl
+dec = lambda ch, key: (ch - key + dl) % dl
+
+
+def vigenere(value, key, func):
+    kl = len(key)
+    value = prepval(value)
+    e = [func(ord(c), ord(key[i % kl])) for (i, c) in value]
+    return ''.join([d[c] for c in e])
+
+
+
+src = 'АБВГ'
+key = 'АБ'
+
+tmp = vigenere(src, key, enc)
+print(tmp)
